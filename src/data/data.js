2f946fc30d4fecbf80b58dffb3ce342a81a9d904
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-12-29 16:31:57 -0600

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -112,7 +112,7 @@ export const portfolio = [
                 icon: faChartLine,
             },
             {
-                name: "Handlebars.js",
+                name: "Ghost",
                 icon: faCode,
             },
             {
